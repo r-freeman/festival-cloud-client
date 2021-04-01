53c9b4dcc441bf03f1ccf0e82f22ca803093de62
Ryan Freeman <N00173767@student.iadt.ie>
2021-04-01 22:37:37 +0100

    Change API url

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'https://7emy2tazo2.execute-api.us-east-1.amazonaws.com/api/'
+    baseURL: 'https://zy8hyxvb7h.execute-api.us-east-1.amazonaws.com/api/'
 })

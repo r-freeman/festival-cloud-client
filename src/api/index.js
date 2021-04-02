7c2c6b88327bd105cdfb3b65727b4c454b24dca2
Ryan Freeman <N00173767@student.iadt.ie>
2021-04-02 17:03:48 +0100

    Added authorization header with token to API client

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,17 @@
-import axios from 'axios'
+import axios from "axios";
+import awsmobile from "@/aws-exports";
+
+const AWS_USER_POOLS_WEB_CLIENT_ID = awsmobile.aws_user_pools_web_client_id;
+const userId = localStorage.getItem(
+  `CognitoIdentityServiceProvider.${AWS_USER_POOLS_WEB_CLIENT_ID}.LastAuthUser`
+);
+const accessToken = localStorage.getItem(
+  `CognitoIdentityServiceProvider.${AWS_USER_POOLS_WEB_CLIENT_ID}.${userId}.accessToken`
+);
 
 export default axios.create({
-    baseURL: 'https://zy8hyxvb7h.execute-api.us-east-1.amazonaws.com/api/'
-})
+  baseURL: "https://zy8hyxvb7h.execute-api.us-east-1.amazonaws.com/api/",
+  headers: {
+    Authorization: `Bearer ${accessToken}`
+  }
+});
